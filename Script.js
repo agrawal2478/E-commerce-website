780a7da9eb35842f64888e79565fa859348afc40
Abhishek Kumar <abhianandabhianand17@gmail.com>
2026-04-24 14:55:25 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,10 @@
+function addedToCart(name) {
+    alert(name + " added to cart!");
+}
+
+function confirmCheckout() {
+    return confirm("Are you sure you want to place the order?");
+}
+function confirmCheckout() {
+    return confirm("Are you sure you want to place the order?");
+}
